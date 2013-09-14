e903c90b1fc52b77a50cea86338336b03533f1c5
Tim Schaub <tim.schaub@gmail.com>
2013-09-14 17:29:04 -0600

    Maintain lib property and make log available

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+var log = require('npmlog');
+
 var Manager = require('./lib/manager').Manager;
 var Server = require('./lib/server').Server;
 var compile = require('./lib/compile');
 
+
 exports.Manager = Manager;
 exports.Server = Server;
 exports.compile = compile;
+exports.log = log;
