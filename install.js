6dc0110ed11021513329ac97e69fb4524eaf8ab4
Tim Schaub <tim.schaub@gmail.com>
2014-01-03 17:36:09 -0700

    Shared config

--- install.js
+++ install.js
@@ -1,37 +1,29 @@
-var crypto = require('crypto');
 var fs = require('fs');
 var path = require('path');
 
-var cc = require('config-chain');
 var download = require('download');
 var log = require('npmlog');
+var rimraf = require('rimraf');
 
-var conf = cc(
-    cc.env('closure_util_'),
-    cc.find('closure-util.json'),
-    path.join(__dirname, 'default-config.json'),
-    {
-      deps: path.join(__dirname, '.deps')
-    });
+var util = require('./lib/util');
+var config = require('./lib/config');
 
 
 /**
  * Configurable log level.
  * @type {string}
  */
-log.level = conf.get('log_level');
+log.level = config.get('log_level');
 
 
-function maybeDownload(url, rootDir, prefix, callback) {
-  var hash = crypto.createHash('sha1');
-  hash.update(url);
-  var dir = path.join(rootDir,
-      prefix + '-' + hash.digest('hex').substring(0, 7));
+function maybeDownload(url, callback) {
+  var dir = util.getDependency(url);
 
   fs.exists(dir, function(exists) {
     if (exists) {
       log.verbose('install', 'Skipping ' + url);
       log.verbose('install', 'To force download delete ' + dir);
+      callback(null, dir);
     } else {
       log.info('install', 'Downloading ' + url);
       var dl = download(url, dir, {extract: true});
@@ -50,22 +42,25 @@ function maybeDownload(url, rootDir, prefix, callback) {
 
       dl.once('close', function() {
         log.info('install', 'Download complete: ' + dir);
+        callback(null, dir);
       });
 
       dl.on('error', function(err) {
-        log.error('install', err);
-        // TODO: delete dir
-        process.exit(1);
+        rimraf(dir, function(rimrafErr) {
+          if (rimrafErr) {
+            log.error('install', rimrafErr);
+          }
+          callback(err);
+        });
       });
     }
   });
 
 }
 
-maybeDownload(conf.get('compiler_url'), conf.get('deps'), 'compiler',
-    function(err, dir) {
-      if (err) {
-        log.error('install', err.message);
-        return process.exit(1);
-      }
-    });
+maybeDownload(config.get('compiler_url'), function(err, dir) {
+  if (err) {
+    log.error('install', err.message);
+    return process.exit(1);
+  }
+});
